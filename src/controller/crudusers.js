df04023ccc31588e5b3fc3a4e641cb514b33f32e
scaicedo27 <45157348+scaicedo27@users.noreply.github.com>
2020-05-20 15:26:38 -0500

    changes

--- src/controller/crudusers.js
+++ src/controller/crudusers.js
@@ -1,20 +1,67 @@
 const pool = require('../connection/connection');
 
-const nameTable = 'user';
+const nameTable = 'users_app';
 
 const model = {
-    ID: 'ID',
-    email: 'email',
+    id: 'id',
+    mail: 'email',
     password: 'password',
 
 };
 
-const createUser = async (req, res) => {
-    const {email,password} = req.body;
-    const response = await pool.query(`INSERT INTO ${nameTable} (email, password) VALUES ($1,$2)`, [email,password]);
-    res.send(email);
+
+async function validateUser(user_mail){
+    const response = await pool.query(`select * from ${nameTable} where email= $1`,[user_mail]);
+    const n = response.rowCount;
+    return n;
+}
+
+const validateUs = async (req, res) =>{
+
+    const response = await pool.query(`select * from ${nameTable} where email= $1`,[req.body.mail]);
+    const n = response.rowCount;
+    res.send(200),res.json(respone.rowCount);
+}
+
+const createUser = async (req, res, next) => {
+    const {mail,password} = req.body;
+    
+    const res_id = await pool.query(`SELECT ${model.id} FROM ${nameTable} WHERE ${model.mail} = '${mail}'`);
+
+    if(res_id.rows[0] == null){
+    const resp = pool.query(`INSERT INTO ${nameTable} (email, password) 
+    SELECT $1, $2
+    WHERE NOT EXISTS (
+        SELECT 1 FROM ${nameTable} WHERE email=$3
+    )`, [mail,password,mail]);
+    res.send('Se agrego correctamente');}
+    else{
+        res.json({
+            message: 'mail invalido'
+        });
+    }
+    
 };
+
+const login = async (req, res) =>{
+    const {mail, password} = req.body;
+
+    const response = await pool.query(`SELECT ${model.id}, ${model.mail}, ${model.password} FROM ${nameTable} where ${model.mail}='${mail}'`);
+    
+    const id_1 = response.rows[0].id;
+    const mail_1 = response.rows[0].email;
+    const pass = response.rows[0].password;
+    
+    res.json({ 
+        id: id_1,
+        mail: mail_1,
+        password: pass
+    });
+       
+}
 
 module.exports = {
     createUser,
+    validateUs,
+    login
 } 
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,19 +3,20 @@ const router = Router();
 
 const { getNameProjectById, getProjects, getProjectById, createProject, deleteProject, updateProject} = require('../controller/crudprojects')
 const { getAssets, getAssetsbyProject, createAsset, deleteAsset, deleteAssets, gettrashbyProject, updateAsset } = require('../controller/crudassets')
-const { createUser} = require('../controller/crudusers')
+const { createUser, validateUs, login} = require('../controller/crudusers')
 
 router.get('/Projects', getProjects);
 router.get('/Projects/:id', getProjectById);
 router.post('/New-Project', createProject);
 router.post('/Del-Project', [deleteAssets, deleteProject]);
 router.post('/New-Asset', createAsset);
-router.post('/New-User', createUser);
+router.post('/new-user', createUser);
+router.post('/validate', validateUs);
+router.post('/login', login );
 router.delete('/Projects/:id', deleteProject);
 router.delete('/Asset/:id', deleteAsset);
 router.get('/assettrash/:id_project', gettrashbyProject);
 router.put('/Assets/:id', updateAsset);
-
 router.get('/Projectn/:id', getNameProjectById);
 router.get('/Assets', getAssets);
 router.get('/Assets/:id_project', getAssetsbyProject);
